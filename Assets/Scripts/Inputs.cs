ab55c520a7feb72da45e70a8acb65450e85be1d5
Ashley Hacker <ashleyhacker0127@gmail.com>
2023-09-28 19:33:19 -0400

    PlayerInput

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Inputs
+{
+    private static PlayerInput _actions;
+    //private static  _owner;
+}
