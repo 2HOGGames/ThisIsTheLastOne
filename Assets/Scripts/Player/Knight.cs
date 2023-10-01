edcd893f2444e9f76d1ab515a2d0338961cd7104
Ashley Hacker <ashleyhacker0127@gmail.com>
2023-10-01 14:22:19 -0400

    Created input scripts (Currently Unfinished)

---
+++ Assets/Scripts/Player/Knight.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+    [SerializeField] private Player knight;
+
+    private void rollKnight()
+    {
+        /*knight.lastRoll = knight.roll(knight.diceAmount);
+
+        knight.lastRoll += knight.might;*/
+    }
+
+}
