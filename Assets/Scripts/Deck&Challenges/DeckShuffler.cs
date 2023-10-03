894b5c42d84b5f4ae3f54704a2182e5d0ca6e226
BeansMage42 <144922891+BeansMage42@users.noreply.github.com>
2023-10-02 21:14:12 -0400

    Created a challenge deck class, changed how the cards are drawn, made the game manager able to draw cards

--- Assets/Scripts/Deck&Challenges/DeckShuffler.cs
+++ Assets/Scripts/Deck&Challenges/DeckShuffler.cs
@@ -5,36 +5,49 @@ using UnityEngine;
 public class DeckShuffler : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private int[] deck = new int[12];
-    [SerializeField] private int[] freeSpaces = new int[12];
+    [SerializeField] private int[] deck = new int[6];//stores the shuffled value of the deck
+    [SerializeField] private int[] freeSpaces = new int[6];//stores chosen spaces
     int randomNum;
     bool spaceTaken = false;
-    private int CardsDrawn = 0;
+    private int CardsDrawn = 1;
+
+    public int card1, card2;
 
     private void Start()
     {
         Shuffle();
-
-        for(int i = 0; i <= 11; i++)
+        firstCard();
+        
+       /* Shuffle();
+        for(int i = 0; i <= deck.Length - 1; i++)
         {
-            Drawnext();
+            Debug.Log(deck[i]);
         }
-    }
-    public void Shuffle()
-    {
-        for(int i = 0; i <= deck.Length -1; i++)
+
+        Drawnext();
+        ReformDeck();
+        Debug.Log("after a card is drawn");
+        for (int i = 0; i <= deck.Length - 1; i++)
         {
-            spaceTaken = false;
-            while(spaceTaken == false)
+            
+            Debug.Log(deck[i]);
+        }*/
+    }
+    public void Shuffle()//shuffles the deck by placing a value from 1-11 in a random index (position in the deck)
+    {
+        for(int i = 0; i <= deck.Length -1; i++)//repeats for each card
+        {
+            spaceTaken = false;//sets the current space as false to initialize the loop
+            while(spaceTaken == false)//loops until the space is taken (a card that hasnt been sorted is sorted)
             {
-                randomNum = Random.Range(0, 12);
+                randomNum = Random.Range(0, 6);//accesss a random index of the array
 
 
-                if(freeSpaces[randomNum] == 0)
+                if(freeSpaces[randomNum] == 0)//if that index hasnt been used yet
                 {
-                    deck[randomNum] = i;
-                    freeSpaces[randomNum] = 1;
-                    spaceTaken = true;
+                    deck[randomNum] = i;//adds the current card to the random chosen index
+                    freeSpaces[randomNum] = 1;//confirms the index has been chosen so the card cant be overridden later
+                    spaceTaken = true;//ends the loop
                 }
 
 
@@ -46,12 +59,46 @@ public class DeckShuffler : MonoBehaviour
 
        
     }
+    private void firstCard()
+    {
+        card1 = deck[0];
+        for (int i = 1; i <= deck.Length - 1; i++)
+        {
+            deck[i - 1] = deck[i];
+            deck[i] = 0;
 
+
+        }
+        CardsDrawn++;
+    }
     public void Drawnext()
     {
-        Debug.Log(deck[CardsDrawn]);
+         card1 = deck[0];
+         card2 = deck[1];
+        
+        Debug.Log("you drew " + card1 + " and " + card2);
+
+        
+
+    }
+    public int[] DrawnCards()
+    {
+        int[] cards = new int[2];
+        Drawnext();
+        cards[0] = card1;
+        cards[1] = card2;
+        return (cards);
+    }
+    public void ReformDeck(int nonChosen)
+    {
+        for (int i = 2; i <= deck.Length - 1; i++)
+        {
+            deck[i - 2] = deck[i];
+            deck[i] = 0;
+
+
+        }
+        deck[5 - CardsDrawn] =  nonChosen;
         CardsDrawn++;
-
-
     }
 }
--- Assets/Scripts/GameLogic/gameManager.cs
+++ Assets/Scripts/GameLogic/gameManager.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class gameManager : MonoBehaviour
 {
     public static gameManager Instance;
+   
+
+    public  DeckShuffler deckShuffler;
+    public ChallengeDeck challengeDeck;
 
     public GameObject[] players = new GameObject[4];
-
+    
     public int currentPlayer;
+    private int[] newCards = new int[2];
+    
     
     void Awake()
     {
@@ -18,5 +24,28 @@ public class gameManager : MonoBehaviour
         Instance = this;
 
     }
+    private void Start()
+    {
+        
+        checkDrawnCard();
+    }
+    private void GameLoop()
+    {
+       
+       
 
+
+
+
+
+    }
+
+    
+    private void checkDrawnCard()
+    {
+        newCards = deckShuffler.DrawnCards();
+
+        Debug.Log(newCards[0]);
+        Debug.Log(newCards[1]);
+    }
 }
