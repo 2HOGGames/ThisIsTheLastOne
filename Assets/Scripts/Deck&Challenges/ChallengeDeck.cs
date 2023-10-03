6316d9f810d39ad534e3cb75bf065d162c24a252
BeansMage42 <144922891+BeansMage42@users.noreply.github.com>
2023-10-03 12:55:20 -0400

    began coding game loop

--- Assets/Scripts/Deck&Challenges/ChallengeDeck.cs
+++ Assets/Scripts/Deck&Challenges/ChallengeDeck.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class ChallengeDeck : MonoBehaviour
 {
-   public void ChosenRoom(int roomNum)
+    int[] roomChallenge = new int[3];
+    int targetNum, stat, numOfRolls, loserPenalty;
+    //target num is the number needed to roll to win the room
+    //stat is the stat being rolled 1 might, 2 magic, 3 none
+    //numOfRolls is how many attempts each player gets
+    //loserPenalty is wether or not there is a penalty for being last
+   public int[] ChosenRoom(int roomNum)
     {
         switch (roomNum)
         {
             case 0:
-                Debug.Log("challenge 1");
+                targetNum = 12;
+                stat = 1;
+                
+                loserPenalty = -1;
                 break;
             case 1:
                 Debug.Log("challenge 2");
@@ -27,14 +36,11 @@ public class ChallengeDeck : MonoBehaviour
                 Debug.Log("challenge 6");
                 break;
         }
+        roomChallenge[0] = targetNum;
+        roomChallenge[1] = stat;
+        roomChallenge[2] = loserPenalty;
+        return (roomChallenge);
     }
 
-    public void MonsterRoom()
-    {
-
-        Debug.Log("monsterDeckchosen");
-
-
-
-    }
+    
 }
